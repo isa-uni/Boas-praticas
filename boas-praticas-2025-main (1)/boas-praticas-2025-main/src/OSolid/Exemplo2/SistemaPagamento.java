ad476ff7ca278ec6ecfdf8cb532714f28aa5e169
Isa_Cristina <163331341+isa-uni@users.noreply.github.com>
2025-03-27 12:35:45 -0300

    Add files via upload

---
+++ boas-praticas-2025-main (1)/boas-praticas-2025-main/src/OSolid/Exemplo2/SistemaPagamento.java
@@ -0,0 +1,19 @@
+package OSolid.Exemplo2;
+
+//Aberto fechado - aberto para extensão fechado para modificação
+
+
+public class SistemaPagamento {
+
+    public void realizarPagamento(double valor, String metodo) {
+        if ("CARTAO".equalsIgnoreCase(metodo)) {
+            System.out.println("Pagamento de R$" + valor + " realizado com CARTÃO.");
+        } else if ("PIX".equalsIgnoreCase(metodo)) {
+            System.out.println("Pagamento de R$" + valor + " realizado via PIX.");
+        } else if ("BOLETO".equalsIgnoreCase(metodo)) {
+            System.out.println("Pagamento de R$" + valor + " realizado via BOLETO.");
+        } else {
+            System.out.println("Método de pagamento não suportado!");
+        }
+    }
+}
